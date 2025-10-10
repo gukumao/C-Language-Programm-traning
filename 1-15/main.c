b2da29d89451a9926d95531d47e8395177c6ff4e
gukumao <gukumao@gmail.com>
2025-10-10 22:18:12 +0800

    1-15

---
+++ 1-15/main.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+int main(void) {
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    printf("<<Temperature>>\r\n");
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+}
+
+
+float get_celsius(float fahr) {
+
+   float celsius = (5.0 / 9.0) * (fahr - 32);
+   return celsius;
+}
+
+
+
